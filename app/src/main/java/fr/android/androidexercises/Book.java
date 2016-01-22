adda681eb621c7f00892a73e8eee1c716fdb1fa0
Killian Saint cricq <killian.40@hotmail.fr>
2016-01-22 11:29:37 +0100

    API call and logging

--- app/src/main/java/fr/android/androidexercises/Book.java
+++ app/src/main/java/fr/android/androidexercises/Book.java
@@ -40,6 +40,16 @@ public class Book {
     }
 
     @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", price='" + price + '\'' +
+                ", cover='" + cover + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,9 +3,18 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    List<Book> books;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +24,30 @@ public class LibraryActivity extends AppCompatActivity {
         // Plant logger cf. Android Timber
         Timber.plant(new Timber.DebugTree());
 
-        // TODO build Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
         // TODO create a service
+        HenriPotierService service = retrofit.create(HenriPotierService.class);
 
         // TODO listBooks()
+        Call<List<Book>> call = service.listBooks();
 
         // TODO enqueue call and display book title
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Response<List<Book>> response, Retrofit retrofit) {
+                books = response.body();
+                Timber.i(books.toString());
+            }
+            @Override
+            public void onFailure(Throwable t) {
+                books = null;
+                Timber.e("error");
+            }
+        });
 
         // TODO log books
 
